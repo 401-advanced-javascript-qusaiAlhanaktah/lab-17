cfca1ac665f7f48c6482c9ce3d6632977301a549
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-25 00:56:13 +0200

    some updating

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,24 +1,23 @@
+/* eslint-disable strict */
+
 'use strict';
+
 
 module.exports = exports = {};
 
-var fileContents = 'helloworld';
+
 
 exports.readFile = (file, cb) => {
   if (file.match(/bad/i)) {
     cb('Invalid File');
   }
-  else {
-    cb(undefined, Buffer.from(fileContents));
-  }
 };
 
+
+
 exports.writeFile = (file, buffer, cb) => {
-  if (file.match(/badTwo/i)) {
-    cb('Invalid File');
-  }
-  else {
-    fileContents = buffer;
-    cb(undefined, true);
+  if (file.match(/bad/i)) {
+    cb('Invalid file');
+    return Buffer.from('File Contents');
   }
 };
